f7b8c8bb06789b3fcfb0395432d20d61e7b0174a
Anna Elola <elola.anna@gmail.com>
2018-01-16 12:31:44 +0100

    first commit

---
+++ src/main/java/backing/LeasingCarBean.java
@@ -0,0 +1,37 @@
+package backing;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Named;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.GenericType;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.client.ClientBuilder;
+
+import dto.Cardto;
+import dto.Customerdto;
+
+@Named
+@ViewScoped
+public class LeasingCarBean implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	private WebTarget leasingCarTarget = ClientBuilder.newClient().target("localhost:8080/leasingcarbackend/leasingcar");
+	
+	public LeasingCarBean() {}
+
+	public List<Cardto> getCarList() {
+		return leasingCarTarget.path("/carlist").request(MediaType.APPLICATION_JSON).get(new GenericType<List<Cardto>>() {});
+	}
+
+	public List<Customerdto> getCustomerList() {
+		return leasingCarTarget.path("/customerlist").request(MediaType.APPLICATION_JSON).get(new GenericType<List<Customerdto>>() {});
+	}
+
+	public List<Cardto> getAvailableCarsList() {
+		return leasingCarTarget.path("/availableOrNotCarList/{isleased}").resolveTemplate("isleased", false).request(MediaType.APPLICATION_JSON).get(new GenericType<List<Cardto>>() {});
+		
+	}
+}
